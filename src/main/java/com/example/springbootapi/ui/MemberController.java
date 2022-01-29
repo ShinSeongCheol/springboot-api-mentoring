473bdf9b079728c29b9ac2a72252a18c6f045932
ShinSeongCheol <cher3725@naver.com>
2022-01-29 22:35:10 +0900

    회원 관리 API 개발  -회원 생성 API  -특정 회원 조회 API  -전체 회원 조회 API  -회원 수정 API  -특정 회원 삭제 API  -전체 회원 삭제 API

--- src/main/java/com/example/springbootapi/ui/MemberController.java
+++ src/main/java/com/example/springbootapi/ui/MemberController.java
@@ -1,5 +1,18 @@
 package com.example.springbootapi.ui;
 
+import com.example.springbootapi.model.Member;
+import com.example.springbootapi.model.MemberCreateRequest;
+import com.example.springbootapi.model.MemberUpdateRequest;
+import com.example.springbootapi.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
 public class MemberController {
 
     /**
@@ -7,7 +20,13 @@ public class MemberController {
      * [POST] /api/members
      * @return
      */
-
+    private final MemberService memberService;
+    
+    @PostMapping("/api/members")
+    public ResponseEntity<Member> createMember(@RequestBody MemberCreateRequest request){
+        Member createMember = memberService.createMember(request);
+        return new ResponseEntity<>(createMember, HttpStatus.OK);
+    }
 
 
     /**
@@ -15,7 +34,11 @@ public class MemberController {
      * [GET] /api/members/{memberId}
      * @return
      */
-
+    @GetMapping("/api/members/{memberId}")
+    public ResponseEntity<Member> findMember(@PathVariable Long memberId){
+        Member findMember = memberService.findMember(memberId);
+        return new ResponseEntity<>(findMember, HttpStatus.OK);
+    }
 
 
     /**
@@ -23,7 +46,11 @@ public class MemberController {
      * [GET] /api/members
      * @return
      */
-
+    @GetMapping("api/members")
+    public ResponseEntity<List<Member>> findAllMember(){
+        List<Member> allMember = memberService.findAllMember();
+        return new ResponseEntity<>(allMember, HttpStatus.OK);
+    }
 
 
     /**
@@ -32,6 +59,11 @@ public class MemberController {
      * @param memberId
      * @return
      */
+    @PatchMapping("api/members/{memberId}")
+    public ResponseEntity<String> updateMember(@PathVariable Long memberId, @RequestBody MemberUpdateRequest request){
+        memberService.updateMember(memberId, request);
+        return new ResponseEntity<>( memberId + "번 회원의 이름이 정상적으로 수정되었습니다.", HttpStatus.OK);
+    }
 
 
 
@@ -41,6 +73,12 @@ public class MemberController {
      * @param memberId
      * @return
      */
+    @DeleteMapping("api/members/{memberId}")
+    public ResponseEntity<String> deleteMember(@PathVariable Long memberId) {
+        memberService.deleteMember(memberId);
+        return new ResponseEntity<>(memberId + "번 회원이 정상적으로 삭제되었습니다.", HttpStatus.OK);
+
+    }
 
 
 
@@ -50,5 +88,9 @@ public class MemberController {
      * @param memberId
      * @return
      */
-
+    @DeleteMapping("api/members")
+    public ResponseEntity<String> deleteAllMember(){
+        memberService.deleteAllMember();
+        return new ResponseEntity<>("모든 회원이 삭제되었습니다.", HttpStatus.OK);
+    }
 }
