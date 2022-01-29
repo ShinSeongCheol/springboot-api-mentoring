5dd2ae273c751adec1e0cdce06209b3c1f9e8069
ShinSeongCheol <cher3725@naver.com>
2022-01-29 22:31:15 +0900

    회원 Entity 개발

--- src/main/java/com/example/springbootapi/model/Member.java
+++ src/main/java/com/example/springbootapi/model/Member.java
@@ -1,4 +1,38 @@
 package com.example.springbootapi.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "member")
+@Builder
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    private String number;
+
+    private String email;
+
+    //MemberCreateRequest -> Member로 변환해주는 과정이 필요
+    public static Member toEntity(MemberCreateRequest request) {
+        return Member.builder()
+                .name(request.getName())
+                .number(request.getNumber())
+                .email(request.getEmail())
+                .build();
+    }
+
+    public void updateName(String name){
+        this.name = name;
+    }
 }
