4d8389ec7b7d217d15183a510e845d396f800dad
ShinSeongCheol <cher3725@naver.com>
2022-01-29 22:33:27 +0900

    회원 생성, 수정 Dto 개발

---
+++ src/main/java/com/example/springbootapi/model/MemberUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.example.springbootapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder //인스턴스를 가독성 좋게 생성할 수있는 어노테이션
+public class MemberUpdateRequest {
+    private String name;
+}
